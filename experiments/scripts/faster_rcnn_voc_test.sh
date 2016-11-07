d94f6397fbb094c56069d66063c2bf65ca068a39
CharlesShang <shangchong90@gmail.com>
2016-11-07 20:39:40 +0800

    delete unnecessary scripts

--- experiments/scripts/faster_rcnn_voc_test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-python ./faster_rcnn/test_net.py \
---gpu 0 \
---weights ./data/VGGnet_fast_rcnn_iter_70000.ckpt \
---imdb voc_2007_test \
---cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
---network VGGnet_test
