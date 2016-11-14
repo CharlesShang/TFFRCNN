d8f3afdebbc42d24c44c3109a0eba7e69a068388
CharlesShang <shangchong90@gmail.com>
2016-11-14 22:41:17 +0800

    adding -D_GLIBCXX_USE_CXX11_ABI=0 to fix roi_pooling.so bugs

--- lib/make.sh
+++ lib/make.sh
@@ -8,7 +8,7 @@ cd roi_pooling_layer
 nvcc -std=c++11 -c -o roi_pooling_op.cu.o roi_pooling_op_gpu.cu.cc \
 	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
 
-g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
+g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=0 -o roi_pooling.so roi_pooling_op.cc \
 	roi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
 cd ..
 
