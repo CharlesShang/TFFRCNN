e81134124dc6f3edffb0e8aa29ff3160d2bd44bf
CharlesShang <shangchong90@gmail.com>
2016-11-20 20:32:04 +0800

    --

--- experiments/eval/voc2007_test.sh
+++ experiments/eval/voc2007_test.sh
@@ -2,7 +2,7 @@
 
 python ./faster_rcnn/test_net.py \
 --gpu 0 \
---weights ./output/faster_rcnn_end2end/voc_2007_trainval/VGGnet_fast_rcnn_iter_65000.ckpt \
+--weights ./output/faster_rcnn_end2end/voc_2007_trainval/VGGnet_fast_rcnn_iter_80000.ckpt \
 --imdb voc_2007_test \
 --cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
 --network VGGnet_test
