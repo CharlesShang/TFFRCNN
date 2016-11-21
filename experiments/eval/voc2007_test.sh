9cec93a27d40a6b80356037aa21989ce563a5809
CharlesShang <shangchong90@gmail.com>
2016-11-21 10:35:25 +0800

    training another round

--- experiments/eval/voc2007_test.sh
+++ experiments/eval/voc2007_test.sh
@@ -6,3 +6,11 @@ python ./faster_rcnn/test_net.py \
 --imdb voc_2007_test \
 --cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
 --network VGGnet_test
+
+
+python ./faster_rcnn/test_net.py \
+--gpu 0 \
+--weights ./output/faster_rcnn_end2end/voc_2007_trainval/VGGnet_fast_rcnn_iter_90000.ckpt \
+--imdb voc_2007_test \
+--cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
+--network VGGnet_test
