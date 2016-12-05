c39d2952f48e4616949ef0823d9c3c869fe445d7
CharlesShang <shangchong90@gmail.com>
2016-12-05 22:01:06 +0800

    add resnet50 test

--- experiments/eval/voc2007_test.sh
+++ experiments/eval/voc2007_test.sh
@@ -10,7 +10,14 @@ python ./faster_rcnn/test_net.py \
 
 python ./faster_rcnn/test_net.py \
 --gpu 0 \
---weights ./output/faster_rcnn_end2end/voc_2007_trainval/VGGnet_fast_rcnn_iter_90000.ckpt \
+--weights ./output/faster_rcnn_end2end/voc_2007_trainval/VGGnet_fast_rcnn_iter_100000.ckpt \
 --imdb voc_2007_test \
 --cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
 --network VGGnet_test
+
+python ./faster_rcnn/test_net.py \
+--gpu 0 \
+--weights ./data/pretrain_model/Resnet_iter_200000.ckpt \
+--imdb voc_2007_test \
+--cfg ./experiments/cfgs/faster_rcnn_end2end_resnet.yml \
+--network Resnet50_test
---
+++ experiments/scripts/faster_rcnn_voc_resnet.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+python ./faster_rcnn/train_net.py \
+--gpu 0 \
+--weights ./data/pretrain_model/Resnet50.npy \
+--imdb voc_2007_trainval \
+--iters 100000 \
+--cfg ./experiments/cfgs/faster_rcnn_end2end_resnet.yml \
+--network Resnet50_train \
+--restore 0
