3c5b979182036bb0ab132602595a361f053a45b7
CharlesShang <shangchong90@gmail.com>
2016-12-06 10:39:30 +0800

    confg

--- experiments/eval/voc2007_test.sh
+++ experiments/eval/voc2007_test.sh
@@ -21,3 +21,12 @@ python ./faster_rcnn/test_net.py \
 --imdb voc_2007_test \
 --cfg ./experiments/cfgs/faster_rcnn_end2end_resnet.yml \
 --network Resnet50_test
+
+
+
+python ./faster_rcnn/test_net.py \
+--gpu 0 \
+--weights ./output/faster_rcnn_end2end_resnet_voc/voc_2007_trainval/VGGnet_fast_rcnn_iter_100000.ckpt \
+--imdb voc_2007_test \
+--cfg ./experiments/cfgs/faster_rcnn_end2end_resnet.yml \
+--network Resnet50_test
