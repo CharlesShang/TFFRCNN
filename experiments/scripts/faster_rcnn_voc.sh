ffbaf1c4baa3c14d8d51ca2800cc1f4ab042a098
CharlesShang <shangchong90@gmail.com>
2016-12-22 17:56:28 +0800

    0712

--- experiments/scripts/faster_rcnn_voc.sh
+++ experiments/scripts/faster_rcnn_voc.sh
@@ -7,3 +7,12 @@ python ./faster_rcnn/train_net.py \
 --cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
 --network VGGnet_train \
 --restore 0
+
+python ./faster_rcnn/train_net.py \
+--gpu 0 \
+--weights ./data/pretrain_model/VGG_imagenet.npy \
+--imdb voc_0712_trainval \
+--iters 100000 \
+--cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
+--network VGGnet_train \
+--restore 0
