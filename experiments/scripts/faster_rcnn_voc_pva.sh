f2d8192dea7e4b3c17537c9599b35eb7cd9ec320
CharlesShang <shangchong90@gmail.com>
2017-02-28 14:21:40 +0800

    pva config

---
+++ experiments/scripts/faster_rcnn_voc_pva.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python ./faster_rcnn/train_net.py \
+--gpu 0 \
+--weights ./data/pretrain_model/pva9.1_preAct_train_iter_1900000.npy \
+--imdb voc_2007_trainval \
+--iters 160000 \
+--cfg ./experiments/cfgs/faster_rcnn_end2end_pva.yml \
+--network PVAnet_train \
+--restore 0
