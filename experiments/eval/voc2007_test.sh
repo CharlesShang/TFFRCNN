5bf2fc9a1733a594974c42e7f1b912019553dfe4
CharlesShang <shangchong90@gmail.com>
2017-03-06 14:48:41 +0800

    --

--- experiments/eval/voc2007_test.sh
+++ experiments/eval/voc2007_test.sh
@@ -10,7 +10,7 @@ python ./faster_rcnn/test_net.py \
 
 python ./faster_rcnn/test_net.py \
 --gpu 0 \
---weights ./output/faster_rcnn_end2end/voc_2007_trainval/VGGnet_fast_rcnn_iter_100000.ckpt \
+--weights ./output/faster_rcnn_voc_vgg/voc_0712_trainval/VGGnet_fast_rcnn_iter_100000.ckpt \
 --imdb voc_2007_test \
 --cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
 --network VGGnet_test
--- experiments/scripts/faster_rcnn_voc.sh
+++ experiments/scripts/faster_rcnn_voc.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-python ./faster_rcnn/train_net.py \
---gpu 0 \
---weights ./data/pretrain_model/VGG_imagenet.npy \
---imdb voc_2007_trainval \
---iters 100000 \
---cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
---network VGGnet_train \
---restore 0
+#python ./faster_rcnn/train_net.py \
+#--gpu 0 \
+#--weights ./data/pretrain_model/VGG_imagenet.npy \
+#--imdb voc_2007_trainval \
+#--iters 100000 \
+#--cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
+#--network VGGnet_train \
+#--restore 0
 
 python ./faster_rcnn/train_net.py \
 --gpu 0 \
@@ -15,4 +15,4 @@ python ./faster_rcnn/train_net.py \
 --iters 100000 \
 --cfg ./experiments/cfgs/faster_rcnn_end2end.yml \
 --network VGGnet_train \
---restore 0
+--restore 1
